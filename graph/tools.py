be4356641ab5e67640872cc09dada73644d2bad5
Uğur KILIÇ <ugur.kilic@erzurum.edu.tr>
2025-03-27 02:34:43 +0300

    Add files via upload

---
+++ graph/tools.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+def get_hop_distance(num_node, edge, hop_size=1):
+    A = np.zeros((num_node, num_node))
+    for i, j in edge:
+        A[j, i] = 1
+        A[i, j] = 1
+
+    hop_dis = np.zeros((num_node, num_node)) + np.inf
+    transfer_mat = [np.linalg.matrix_power(A, d) for d in range(hop_size + 1)]
+    arrive_mat = (np.stack(transfer_mat) > 0)
+    for d in range(hop_size, -1, -1):
+        hop_dis[arrive_mat[d]] = d
+    return hop_dis
+
+def normalize_digraph(A): 
+    Dl = np.sum(A, 0)
+    h, w = A.shape
+    Dn = np.zeros((w, w))
+    for i in range(w):
+        if Dl[i] > 0:
+            Dn[i, i] = Dl[i] ** (-1)
+    AD = np.dot(A, Dn)
+    return AD
